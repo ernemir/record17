6919cad39e625a3064cca1c498a783e1049a3d33
totidelfin <toti.delfin@gmail.com>
2015-09-14 21:06:39 -0300

    tabulaciones en comentarios

--- record17.sql
+++ record17.sql
@@ -62,7 +62,7 @@ CREATE  TYPE efectiveAcquisitionSpectrum AS ENUM ('NIR','DEFINED','VIS','RED','U
 /* 17.028 DME "damaged or missing eye" 
 *	ANSI/NIST-ITL 1-2011 Update: 2013 pag. 410-411
 *	MA = missing or artificial eye,
-* UC = unable to capture image
+* 	UC = unable to capture image
 */
 CREATE TYPE damagedOrMissingEye AS ENUM ('MA','UC');
 
@@ -70,10 +70,10 @@ CREATE TYPE damagedOrMissingEye AS ENUM ('MA','UC');
 /* 17.030 DMM "device monitoring mode" 
 *	ANSI/NIST-ITL 1-2011 Update: 2013 pag. 411 & 64-65
 *	CONTROLLED = Operator physically controls the subject to acquire the biometric sample;
-* ASSITED = Person available to provide assistance to subject submitting the biometric;
-* OBSERVED = Person present to observe operation of the device but provides no assistance;
-* UNATTENDED =  No one is present to observe or provide assistance;
-* UNKNOWN = No information is known;
+* 	ASSITED = Person available to provide assistance to subject submitting the biometric;
+* 	OBSERVED = Person present to observe operation of the device but provides no assistance;
+* 	UNATTENDED =  No one is present to observe or provide assistance;
+* 	UNKNOWN = No information is known;
 */
 CREATE TYPE deviceMonitoringMode AS ENUM ('CONTROLLED','ASSISTED','OBSERVED','UNATTENDED','UNKNOWN');
 
@@ -81,8 +81,8 @@ CREATE TYPE deviceMonitoringMode AS ENUM ('CONTROLLED','ASSISTED','OBSERVED','UN
 /* 17.031 IAP "subject aqcuisition profile iris" 
 *	ANSI/NIST-ITL 1-2011 Update: 2013 pag. 411 & 87-88
 *	20 = iris diameter: >=140 pixels, eyes: >=1, exposure time: =<33 ms;
-* 30 = iris diameter: >=170 pixels, eyes: >=1, exposure time: =<15 ms;
-* 40 = iris diameter: >=210 pixels, eyes: 2, exposure time: =<10 ms;
+* 	30 = iris diameter: >=170 pixels, eyes: >=1, exposure time: =<15 ms;
+* 	40 = iris diameter: >=210 pixels, eyes: 2, exposure time: =<10 ms;
 */
 CREATE TYPE subjectAcquisitionProfileIris AS ENUM ('20','30','40');
 
@@ -90,9 +90,9 @@ CREATE TYPE subjectAcquisitionProfileIris AS ENUM ('20','30','40');
 /* 17.032 ISF "iris storage format"
 *	ANSI/NIST-ITL 1-2011 Update: 2013 pag. 412-413
 *	1 = Unconstrained image size (Recommended),
-* 2 = Raw: 640x480 (Recommended),
-* 3 = Cropped (Required),
-* 7 = Cropped & masked (Required),
+* 	2 = Raw: 640x480 (Recommended),
+* 	3 = Cropped (Required),
+* 	7 = Cropped & masked (Required),
 */
 CREATE TYPE irisStorageFormat AS ENUM ('1','2','3','7');
 
@@ -100,8 +100,8 @@ CREATE TYPE irisStorageFormat AS ENUM ('1','2','3','7');
 /* 17.033-036 BDC "boundary definition codes"
 *	ANSI/NIST-ITL 1-2011 Update: 2013 pag. 413-414 & 106
 *	C = Circle (2 points),
-* E = Elipse (3 points),
-* P = Polygon (between 3 and 99 points),
+* 	E = Elipse (3 points),
+* 	P = Polygon (between 3 and 99 points),
 */
 CREATE TYPE boundaryDefinitionCodes AS ENUM ('C','E','P');
 
@@ -109,9 +109,9 @@ CREATE TYPE boundaryDefinitionCodes AS ENUM ('C','E','P');
 /* 17.037 OCY "occlusion opacity"
 *	ANSI/NIST-ITL 1-2011 Update: 2013 pag. 414 & 106
 *	T = Total,
-* I = Interference,
-* L = Partial Light,
-* S = Partial Shadow,
+* 	I = Interference,
+* 	L = Partial Light,
+* 	S = Partial Shadow,
 */
 CREATE TYPE occlusionOpacity AS ENUM ('T','I','L','S');
 
@@ -119,11 +119,11 @@ CREATE TYPE occlusionOpacity AS ENUM ('T','I','L','S');
 /* 17.037 OCT "occlusion type"
 *	ANSI/NIST-ITL 1-2011 Update: 2013 pag. 414 & 106
 *	L = Lashes,
-* H = Head covering,
-* S = Specular,
-* C = Shadow,
-* R = Reflection,
-* O = Other
+* 	H = Head covering,
+* 	S = Specular,
+* 	C = Shadow,
+* 	R = Reflection,
+* 	O = Other
 */
 CREATE TYPE occlusionType AS ENUM ('L','H','S','C','R','O');
 
